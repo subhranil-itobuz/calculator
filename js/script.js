40c41db18d45d7896aa6fe4214a366a9edcff2d4
Subhranil Das <subhranil@itobuz.com>
2025-01-14 12:19:43 +0530

    feat: calculator basic features done successfully

--- js/script.js
+++ js/script.js
@@ -1,98 +1,90 @@
-const input = document.getElementById('input')
-const operatorBtn = document.querySelectorAll('.operator-btn')
-const operandBtn = document.querySelectorAll('.operand-btn')
-const clearBtn = document.getElementById('clearBtn')
-const signBtn = document.getElementById('signBtn')
-const deleteBtn = document.getElementById('deleteBtn')
-const evalBtn = document.getElementById('evalBtn')
-const percentageBtn = document.getElementById('percentageBtn')
-const decimalBtn = document.getElementById('decimalBtn')
+const input = document.getElementById("input");
+const operatorBtn = document.querySelectorAll(".operator-btn");
+const operandBtn = document.querySelectorAll(".operand-btn");
+const clearBtn = document.getElementById("clearBtn");
+const signBtn = document.getElementById("signBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const evalBtn = document.getElementById("evalBtn");
+const percentageBtn = document.getElementById("percentageBtn");
+const decimalBtn = document.getElementById("decimalBtn");
 
 let operand1 = 0;
 let operand2 = 0;
-let operator = '';
-let result = 0;
+let operator = "";
 
-input.value = ''
+input.value = "";
 
-evalBtn.disabled = input.value.length < 0 ? true : false;
-
-clearBtn.addEventListener('click', () => {
-  input.value = ''
+clearBtn.addEventListener("click", () => {
+  input.value = "";
   operand1 = 0;
   operand2 = 0;
-  operator = '';
-})
-
-deleteBtn.addEventListener('click', () => {
-  if (input.value === 'Infinity' || input.value === 'NaN')
-    clearBtn.click()
-  else
-    input.value = input.value.substring(0, input.value.length - 1)
-})
-
-signBtn.addEventListener('click', () => {
-  input.value = -Number(input.value)
-  operand1 = input.value
-})
-
-operandBtn.forEach(element => {
-  element.addEventListener('click', () => {
-    if (operator === '') {
-      input.value += element.innerHTML
-      operand1 = input.value
-    }
-    else {
-      input.value += element.innerHTML
-      operand2 = input.value
-    }
-  })
+  operator = "";
 });
 
-operatorBtn.forEach(element => {
-  element.addEventListener('click', () => {
-    input.value = element.innerHTML
-    operator = input.value
-  })
-})
+deleteBtn.addEventListener("click", () => {
+  if (input.value === "Infinity" || input.value === "NaN" || input.value === '-Infinity') clearBtn.click();
+  else input.value = input.value.substring(0, input.value.length - 1);
+});
 
-evalBtn.addEventListener('click', () => {
-  operand2 = operand2.toString().slice(1)
+signBtn.addEventListener("click", () => {
+  if (operator === "") {
+    input.value = -Number(input.value);
+    operand1 = input.value;
+  }
+});
+
+operandBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (operator === "") {
+      input.value += element.innerHTML;
+      operand1 = input.value;
+    } else {
+      input.value += element.innerHTML;
+      operand2 = input.value;
+    }
+  });
+});
+
+operatorBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    input.value = element.innerHTML;
+    operator = input.value;
+  });
+});
+
+evalBtn.addEventListener("click", () => {
+  let result = 0;
+  operand2 = operand2.toString().slice(1);
   switch (operator) {
-    case '+':
-      result = Number(operand1) + Number(operand2)
+    case "+":
+      result = Number(operand1) + Number(operand2);
       break;
-    case '-':
-      result = Number(operand1) - Number(operand2)
+    case "-":
+      result = Number(operand1) - Number(operand2);
       break;
-    case '*':
-      result = Number(operand1) * Number(operand2)
+    case "*":
+      result = Number(operand1) * Number(operand2);
       break;
-    case '/':
-      result = Number(operand1) / Number(operand2)
+    case "/":
+      result = Number(operand1) / Number(operand2);
       break;
 
     default:
-      result = Number(operand1)
+      result = Number(operand1);
   }
 
-  if (!Number.isInteger(result))
-    result = result.toFixed(2)
-  
-  input.value = result
+  if (!Number.isInteger(result)) result = result.toFixed(2);
+
+  input.value = result;
   operand1 = result;
-  operator = ''
-}
-)
+  operator = "";
+});
 
-percentageBtn.addEventListener('click', () => {
+percentageBtn.addEventListener("click", () => {
   input.value = Number(operand1) / 100;
-  operand1 = input.value
-}
-)
+  operand1 = input.value;
+});
 
-decimalBtn.addEventListener('click', () => {
-  if (!input.value.includes('.'))
-    input.value += '.'
-}
-)
+decimalBtn.addEventListener("click", () => {
+  if (!input.value.includes(".")) input.value += ".";
+});
